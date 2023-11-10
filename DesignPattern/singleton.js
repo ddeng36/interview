4e207cd3844a655d24552ccdf294f11cbb2f2dae
dengdingchao <dengdingchao@baidu.com>
2023-11-10 16:47:11 +0800

    1

--- DesignPattern/singleton.js
+++ DesignPattern/singleton.js
@@ -1,4 +1,6 @@
 // 保证一个类仅有一个实例，并提供一个访问它的全局访问点。
+// 优点：减少内存开销，避免对资源的多重占用，设置全局访问点，严格控制访问。
+// 缺点：没有接口，扩展困难，不利于测试。
 const Singleton = (function () {
   let instance;
 
@@ -18,3 +20,5 @@ const Singleton = (function () {
 const instance1 = Singleton.getInstance();
 const instance2 = Singleton.getInstance();
 console.log(instance1 === instance2); // true
+
+
